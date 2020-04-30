f5d7b4a5ed09bc7f78e8e5dc721e7ebe2a13d0b1
Abhinav Kumar <abhinavmalviya1992@gmail.com>
2020-04-30 23:21:22 +0200

    updated

--- src/main/java/com/qa/util/Constant.java
+++ src/main/java/com/qa/util/Constant.java
@@ -2,7 +2,7 @@ package com.qa.util;
 
 public class Constant {
 
-	public static final String LOGIN_PAGE_TITLE="HubSpot Loginc";
+	public static final String LOGIN_PAGE_TITLE="HubSpot Login";
 	public static final String HOME_PAGE_TITLE="Account Setup | HubSpot";	
 	public static final String ACCOUNT_NAME="Abhinav";
 	public static final String PROFILE_PAGE_TITLE="User Preferences | HubSpot";
